7a38bbda8f285bc09002ba53e7ddd4a500f6afd6
Dave-Rushabh <rnd2412@gmail.com>
2024-05-04 15:22:18 +0530

    font style initialized, with common css configs

--- src/pages/searchJob/index.jsx
+++ src/pages/searchJob/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./index.css";
 
 const SearchJob = () => {
   return (
-    <>
-      <div>SearchJob</div>
-    </>
+    <div className="search-job-page-container container">
+      <div className="filters-section">Filters will be added later</div>
+      <div className="job-listing-section">Cards will be added later</div>
+    </div>
   );
 };
 
